70a4e91734a05324a6a349d707c4fc152542c656
trungnt256 <trungnt256@gmail.com>
2022-08-14 23:00:52 +0700

    Xay dung UI Header 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,28 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://images.viblo.asia/60x60/35ae4c25-3806-4f37-b089-fb2af1ec226d.jpg"
+        alt="kentrung"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>KenTrung</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>@kentrung256</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
